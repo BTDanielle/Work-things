2fd97b9d0d8f70776802febd43de08cd79e82224
BTDanielle <84688274+BTDanielle@users.noreply.github.com>
2023-09-28 14:37:03 +1000

    progress

--- Dr Dona/index.js
+++ Dr Dona/index.js
@@ -2,12 +2,10 @@ const hamburger = document.querySelector(".hamburger");
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
-// const mobilelinks = document.querySelector(".mobile-links");
 
 hamburger.addEventListener("click", () => {
     // alert("clicked");
     bar1.classList.toggle("menuanim1");
     bar2.classList.toggle("menuanim2");
     bar3.classList.toggle("menuanim3");
-    // mobilelinks.classList.toggle("openmobilemenu");
 });
